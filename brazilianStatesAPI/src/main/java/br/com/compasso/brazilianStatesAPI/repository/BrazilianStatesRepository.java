6ca24e1e4c29c53a77542844c0225552898f90dd
Otavio de Mello <otaviodemello15@gmail.com>
2022-03-20 17:15:50 -0300

    Limpeza no código

--- brazilianStatesAPI/src/main/java/br/com/compasso/brazilianStatesAPI/repository/BrazilianStatesRepository.java
+++ brazilianStatesAPI/src/main/java/br/com/compasso/brazilianStatesAPI/repository/BrazilianStatesRepository.java
@@ -12,10 +12,6 @@ public interface BrazilianStatesRepository extends JpaRepository<BrazilianState,
 
     //Query method that filters states by zone
     List<BrazilianState> findByCountryZone(CountryZone countryZone, Sort sort);
-    //Query method that order states by population amount, from the highest value to lowes
-    List<BrazilianState> findByOrderByStatePopulationAmountDesc();
-    //Query method that order states by state area, from the highest value to lowes
-    List<BrazilianState> findByOrderByStateAreaDesc();
     //Query method that fetches a state by id
     Optional<BrazilianState> findById(Long stateID);
 }
